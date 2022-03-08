1b9753bd7f2d5c30daf47b2100a087441933e283
Mahboob Elahi <mahboobelahi93@gmail.com>
2022-03-08 11:12:02 +0200

    function calls were updated

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from FANUC.workstation import RobotCell
 from FANUC.configurations import ROBOT_ID,ROBOT_NAME
 import threading,time
+from FANUC.UtilityFunction import start_camera_cycle
 
 
 if __name__ == '__main__':
@@ -8,14 +9,15 @@ if __name__ == '__main__':
     #access token
     _fanuc.getAccessToken()
     #refresh token
-
-
+    refreshToken=threading.Thread(target= _fanuc.refreshToken)
+    refreshToken.daemon=True
+    refreshToken.start()
 
     """
         Register robot to ZDMP-DAQ component. It is one time API call and can be
         done using any REST-client like Postman etc. 
     """
-    registerRobot=threading.Timer(1, _fanuc.register_device)
+    registerRobot=threading.Timer(0.5, _fanuc.register_device)
     registerRobot.daemon=True
     registerRobot.start()
     """
@@ -23,11 +25,27 @@ if __name__ == '__main__':
         ZDMP-Service and Message Bus when ever a measurement from device is recorded 
         on ZDMP-DAQ component. For disabling this feature omit last parameter
     """
-    subscribeToAsync = threading.Timer(0.5, _fanuc.sub_or_Unsubscribe_DataSource)
+    subscribeToAsync = threading.Timer(0.5, _fanuc.sub_or_Unsubscribe_DataSource, args=(True,))
     subscribeToAsync.daemon = True
     subscribeToAsync.start()
+    """
+        Connect to robot socket server and get Positional data
+        start FTP connect to download latest picture of workspace
+        publish data to MsgBus as well as sends to DAQ    
+    """
+    SocketComm = threading.Timer(0.5, _fanuc.start_socket_server)
+    SocketComm.daemon = True
+    SocketComm.start()
+
+    #start camera cycle 
+    camCycle = threading.Timer(4, start_camera_cycle, args=(_fanuc,))
+    camCycle.daemon = True
+    camCycle.start()
     
     # start Flask server for robot
     #wait for execution of previous functions
-    time.sleep(5)
-    _fanuc.runApp()
+    runApp = threading.Timer(2, _fanuc.runApp())
+    runApp.daemon = True
+    runApp.start()
+
+    
