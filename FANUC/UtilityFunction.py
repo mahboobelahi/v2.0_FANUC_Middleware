b7a74d983c48ee28c1f9a443d8a722f88fb3847a
Mahboob Elahi <46259412+mahboobelahi@users.noreply.github.com>
2022-03-22 13:41:00 +0200

    update_POS modified

--- FANUC/UtilityFunction.py
+++ FANUC/UtilityFunction.py
@@ -28,15 +28,23 @@ def IMG_bytes_to_JSON(image,JSON_DATA):
        
 #Robot information cycle 1
 def start_camera_cycle(obj):
-    req = requests.get(f'{ORCHESTRATOR_URL}', params={"CMD":199})
-    obj.sendEvent(f'CameraCycle',f'CameraCycle_{obj.get_IMG_Count()} has started.')
-    print(f'[X-UFF] Status Code: {req.status_code}')  
+    try:
+        req = requests.get(f'{ORCHESTRATOR_URL}', params={"CMD":199})
+        obj.sendEvent(f'CameraCycle',f'CameraCycle_{obj.get_IMG_Count()} has started.')
+        print(f'[X-UH-CamCycle] Status Code: {req.status_code}') 
+        #start_camera_cycle(self)
+        req= requests.get(f'{ORCHESTRATOR_URL}',params={"CMD":199})
+        print(f'[X-UH] {req.status_code}')
+        time.sleep(1)
+    except requests.exceptions.RequestException as err:
+        print ("[X-W-SUD] OOps: Something Else",err)
+ 
 
 def parsed_Roki_Msg(Roki_Msg,self):
     #de-serialize incomming JSON string to python dictionary
     #Msg_dict = json.loads(Roki_Msg)
     IK_solutions = Roki_Msg.get("InverseKinematicSolutions")
-    id =100
+    id =100 #for jPOS
     if IK_solutions:
         for j in IK_solutions:
             key,j_angles = list(j.items())[0]
@@ -50,7 +58,7 @@ def parsed_Roki_Msg(Roki_Msg,self):
                 ("J6_angel_str",j_angles[5])])
             print(f'[X-UH] {payload}')
             try:
-                req= requests.get(f'{UPDATE_POS_REG}',params=payload)
+                req= requests.get(f'{UPDATE_POS_REG}z_getRokiPOS',params=payload)
                 print(f'[X-UH] {req.text}')
                 if int(req.text) == 200:
                     print("[X-UH] Reachable POS....")
@@ -88,20 +96,78 @@ def parsed_Roki_Msg(Roki_Msg,self):
 
 
 #update robot position
-def update_POS(POS):
+def update_POS(Roki_Msg,self):
+
+    #error checking must be implemented
+    if (Roki_Msg.get("errors") !=None) and (len(Roki_Msg.get("errors"))):
+        print(f'[X-uPOS] {Roki_Msg.get("errors")}')
+        #start camera cycle
+        #threading.Thread(target=start_camera_cycle,args=(self,)).start()
+        self.sendEvent('POS',f'POS-Reg is not updated: {Roki_Msg.get("errors")}')
+        return
+
+    Pose = Roki_Msg.get("Pose")
+    id =94 # cartPos
+    if Pose:
+            payload = dict([
+                ("id",id),
+                ("XX",Pose.get("xyz")[0]),
+                ("YY",Pose.get("xyz")[1]),
+                ("ZZ",Pose.get("xyz")[2]),
+                ("WW",Pose.get("wpr")[0]),
+                ("PP",Pose.get("wpr")[1]),
+                ("RR",Pose.get("wpr")[2])])
+            print(f'[X-UH-uPOS]>>> {payload}')
+            try:
+                req= requests.get(f'{UPDATE_POS_REG}z_CART_POS',params=payload)
+                print(f'[X-UH-uPOS] {req.text}')
+                if int(req.text) == 200:
+                    print("[X-UH-uPOS] Reachable POS....")
+                    time.sleep(0.1)
+                    self.sendEvent('POS',f'POS-Reg is updated with IK Solution {Pose}')
+                    self.sendEvent('RobotCycle', 'Robot cycle is initiatiated.')
+                    req= requests.get(f'{ORCHESTRATOR_URL}',params={"CMD":198})
+                    print(f'[X-UH--uPOS] Updating POS... {req.status_code}')
+                else:
+                    print("[X-UH-uPOS] Not-Reachable POS....")
+                    self.sendEvent('RobotCycle', 'Robot cycle not initiatiated.')
+                    self.sendEvent('POS',f'POS-Reg not updated for IK Solution {Pose} coz, Not Reachable!')
+                    
+                    try:
+                        
+                        #start_camera_cycle(self)
+                        req= requests.get(f'{ORCHESTRATOR_URL}',params={"CMD":199})
+                        print(f'[X-UH-Cam] {req.status_code}')
+                        #time.sleep(1)
+                    except requests.exceptions.RequestException as err:
+                        print ("[X-UH-uPOS] OOps: Something Else",err)
+            except requests.exceptions.RequestException as err:
+                print ("[X-UH-uPOS] OOps: Something Else",err)
+    else:
+        try:
+            print("[X-UH-uPOS] No IK Solutions....")
+            print("[X-UH-uPOS] Now starting camera cycle...")
+            
+            #start_camera_cycle(self)
+            threading.Thread(target=start_camera_cycle,args=(self,)).start()
+            # req= requests.get(f'{ORCHESTRATOR_URL}',params={"CMD":199})
+            print(f'[X-UH] {req.status_code}')
+            time.sleep(1)
+        except requests.exceptions.RequestException as err:
+            print ("[X-W-SUD] OOps: Something Else",err)
     #[float(val) for val in json.loads(POS).values()]
-    id= 100
-    (XX,YY,ZZ,WW,PP,RR) = ([float(val) for val in json.loads(POS).values()])
-    payload =dict([ ("id",id),
-                    ("XX",XX),("YY",YY),("ZZ",ZZ),
-                    ("WW",WW),("PP",PP),("RR",RR)
-                    ])
+    # id= 100
+    # (XX,YY,ZZ,WW,PP,RR) = ([float(val) for val in json.loads(POS).values()])
+    # payload =dict([ ("id",id),
+    #                 ("XX",XX),("YY",YY),("ZZ",ZZ),
+    #                 ("WW",WW),("PP",PP),("RR",RR)
+    #                 ])
     
-    req= requests.get(f'{UPDATE_POS_REG}',params=payload)
-    print(f'[X-UH] {req.url}')
-    time.sleep(0.1)
-    req= requests.get(f'{ORCHESTRATOR_URL}',params={"CMD":198})
-    print(f'[X-UH] {req.url}')
+    # req= requests.get(f'{UPDATE_POS_REG}',params=payload)
+    # print(f'[X-UH] {req.url}')
+    # time.sleep(0.1)
+    # req= requests.get(f'{ORCHESTRATOR_URL}',params={"CMD":198})
+    # print(f'[X-UH] {req.url}')
 
   
 
