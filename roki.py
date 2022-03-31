ad51aa67e9d58e6be75dea1a04a35f0f4666ec43
Mahboob Elahi <46259412+mahboobelahi@users.noreply.github.com>
2022-03-31 19:12:23 +0300

    last

--- roki.py
+++ roki.py
@@ -2,18 +2,21 @@ import cv2,json
 import base64 as b64
 from pprint import pprint
 
-# with open("fromFANUC.json") as jsonFile:
-#     jsonObject = json.load(jsonFile)
-#     img_array=cv2.imread('IMG1.png')
-#     jsonObject[0].get("ImageData")["Picture"]= img_array.tolist()
-#     jsonObject[0].get("ImageData")["txtPicture"] = str(b64.standard_b64encode(img_array),'utf-8')
-#     with open('fromFANUC.json', 'w') as outfile:
-#         json.dump(jsonObject, outfile,)
-    #pprint(img_array.tolist())
-
 with open("fromFANUC.json") as jsonFile:
     jsonObject = json.load(jsonFile)
-    img_array=cv2.imread('IMG1.png')
-    cv2.imshow('ss',img_array)
-    cv2.waitKey(0) # waits until a key is pressed
-    cv2.destroyAllWindows() # destroys the window showing image
+    img_array=cv2.imread('IMG00.png', cv2.IMREAD_GRAYSCALE)
+    #jsonObject.get("ImageData")["Picture"]= img_array.tolist()
+    jsonObject.get("ImageData")["txtPicture"] = str(b64.standard_b64encode(img_array),'utf-8')
+    jsonObject.get("ImageData")["Picture"]= img_array.tolist()
+    with open('fromFANUC.json', 'w') as outfile:
+        json.dump(jsonObject, outfile,
+                indent=4
+                )#separators=(',',': ')
+    #pprint(img_array.tolist())
+
+# with open("fromFANUC.json") as jsonFile:
+#     jsonObject = json.load(jsonFile)
+#     img_array=cv2.imread('IMG1.png', cv2.IMREAD_GRAYSCALE)
+#     cv2.imshow('ss',img_array)
+#     cv2.waitKey(0) # waits until a key is pressed
+#     cv2.destroyAllWindows() # destroys the window showing image
